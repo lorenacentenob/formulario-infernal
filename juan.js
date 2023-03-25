bf1288bde57fe2c7e247cdd5d38aa026a6edeb90
Juan Marín <phreak777@gmail.com>
2023-03-25 03:49:37 +0100

    La anterior tenía polución... YOLO

--- juan.js
+++ juan.js
@@ -7,9 +7,7 @@ function cargar() {
     let lora = document.getElementsByTagName('input');
     for (let i = 0; i < lore.length; i++ ) {
         let mynode = document.createElement('td');
-        console.log(lore[i]);
         mynode.innerHTML = '<td>' + lora[i].value + '</td>';
-        console.log(mynode);
         mytable.appendChild(mynode);
     }
 }
